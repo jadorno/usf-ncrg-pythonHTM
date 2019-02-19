22a1ffa57c6387463869f74a987eeddef16cdf39
jannireddyk@gmail.com <jannireddy@7>
2019-02-18 22:19:52 -0500

    coded and tested main.py

--- SpatialPooler.py
+++ SpatialPooler.py
@@ -1,2 +1,17 @@
+import numpy as np
+
 import config
+
+def spatialPooler(encodedInput, learnP, displayFlag):
+    """Spatial pooler"""
+    print(encodedInput.shape)
+    print(encodedInput)
+    # flip row vector input into column vector, if needed.
+    if encodedInput.shape[0] == 1:
+        encodedInput = encodedInput.T
+    print(encodedInput.shape)
+
+    # Compute overlap
+
+
 
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
+import numpy as np
+
 import config
 import initialize
 from encoderNAB import encoderNAB
+import SpatialPooler
 
 def main(inFile, outFile, displayflag, learnFlag, learntDataFile):
-    config.SP['width'] = 21
-    en = encoderNAB()
-    en.encode(inFile, config.SP['width'])
-    print("before rt")
-    print(config.data)
-    print("After encode function")
-    initialize.initialize()
-    # initialize
+    if learnFlag:
+        config.SP['width'] = 21
+        en = encoderNAB()
+        en.encode(inFile, config.SP['width'])
+        print("before rt")
+        print(config.data)
+        print("After encode function")
+        initialize.initialize()
+        # Learning mode for spatial pooler
+        # We train the spatial pooler in a separate step from sequence memory
+        print('Learning sparse distributed representations using spatial pooling...')
+        # Use the first 15 percent of the data (upto a maximum of 750) samples for training the spatial pooler.
+        trN = min(750, round(0.15 * config.data['N']))
+        for iteration in range(0, trN):
+            x = np.array([])  # construct the binary vector x for each measurement from the data fields
+            for i in range(0, len(config.data['fields'])):
+                j = config.data['fields'][i]
+                x = np.append(x, config.data['code'][j][config.data['value'][j][iteration], :])
+            xSM = SpatialPooler.spatialPooler(np.reshape(x, (1, len(x))), True, False)
+        print('learning sparse distributed representations using spatial pooling... Done.')
+
+
+
+
+
 
 
 if __name__ == '__main__':
